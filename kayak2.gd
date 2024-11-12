482c498f1ff461507dfccd23f3c3eba678bff57a
Bazemot1 <145870083+Bazemot1@users.noreply.github.com>
2024-11-12 13:15:22 -0500

    Kayak_Preset_Data
    Fixed the Kayak2 script so that it now can take read and write all of the data into arrays. When using this file please make sure that The TestDataSet.csv is in the directory in your Godot environment.

--- kayak2.gd
+++ kayak2.gd
@@ -1,27 +1,37 @@
 # Kayak_movement https://forum.godotengine.org/t/using-await-in-a-loop-only-functions-once/42998/3 
 #user:zdrmlpzdrmlp
 #https://docs.godotengine.org/en/stable/classes/class_fileaccess.html
+#https://www.reddit.com/r/godot/comments/rfwuj1/how_can_i_properly_read_this_large_csv_file/
 extends CharacterBody3D
 var rotation_speed=2
-var gravity_speed=9.8
-var speed_scale=200
+var gravity_speed=1000
+var speed_scale=40
 var direction=Vector3.ZERO
 var movement_speed=Vector3.ZERO
 var forward_movement_vetor=Vector3.ZERO
-var velocities=[]
+var csv=[]
+var time=[]
+var time_update
+var x_array=[]
+var x_update
+var z=[]
+var z_update
+var velocity_array=[]
 var counter=0
 var wait=0.0
 
 func _ready():
 	read_kayak_data()
+	write_kayak_data()
 func kayak_movement(delta):
 	wait+=delta
-	if wait>= 1.0 and counter < velocities.size():
-		direction.x=velocities[counter]
-		print(velocities[counter])
+	if wait>=time[counter] and counter < velocity_array.size():
+		direction.x=velocity_array[counter]
+		z_update=z[counter]
+		x_update=x_array[counter]
+		time_update=time[counter]
+		print("Velocity of Kayak:",velocity_array[counter])
 		counter+=1
-		wait=0.0
-
 func _physics_process(delta):
 	kayak_movement(delta)
 	if Input.is_action_pressed("move_left"):
@@ -35,12 +45,32 @@ func _physics_process(delta):
 	velocity=movement_speed
 	move_and_slide()
 func read_kayak_data():
-	var file = FileAccess.open('res://Velocities.csv',FileAccess.READ)
+	var file = FileAccess.open('res://TestDataSet.csv',FileAccess.READ)
 	while not file.eof_reached():
-		var kayak_data=file.get_csv_line()
-		var vel=float(kayak_data[0])
-		velocities.append(vel)
+		var kayak_data=file.get_csv_line(",")
+		csv.append(kayak_data)
 	file.close()
-	return velocities
+	csv.pop_back()
+	return csv
+func write_kayak_data():
+	var csv_data=csv.duplicate()
+	csv_data.pop_front()
+	var headers=Array(csv[0])
+	print(headers)
+	var velocity_header=headers.find("vel")
+	var x_header=headers.find('x')
+	var z_header=headers.find('z')
+	var time_header=headers.find('time')
+	for i in csv_data:
+		velocity_array.append(float(i[velocity_header]))
+		x_array.append(float(i[x_header]))
+		z.append(float(i[z_header]))
+		time.append(float(i[time_header]))
+	
 		
+		
+
+	
+	
+
 
